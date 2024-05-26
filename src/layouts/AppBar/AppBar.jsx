beca34817e2d091b1e3dcb610ee5d6eea6a84e9c
Sito8943 <sito8943@gmail.com>
2024-05-26 08:04:56 -0400

    better app bar

--- src/layouts/AppBar/AppBar.jsx
+++ src/layouts/AppBar/AppBar.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 import { Link, useLocation } from "react-router-dom";
 
 // image
@@ -7,7 +9,10 @@ import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 // images
 import noUser from "../../assets/user-no-image.webp";
 
-function AppBar() {
+function AppBar(props) {
+  const { t } = useTranslation();
+  const { openMenu } = props;
+
   const location = useLocation();
 
   return (
@@ -15,18 +20,32 @@ function AppBar() {
       <div className="flex items-center justify-between h-full w-full px-3 gap-3">
         <div className="flex items-center justify-start gap-3">
           {location.pathname != "/" && (
-            <Link to="/">
+            <Link
+              name={t("_common:names.buttons.back")}
+              aria-label={t("_common:ariaLabels.buttons.back")}
+              to="/"
+            >
               <FontAwesomeIcon icon={faChevronLeft} />
             </Link>
           )}
           <h1 className="font-bold text-4xl sm:text-xl text-plight logo">
-            IMC Calculator
+            EatSmart
           </h1>
         </div>
-        <img className="w-10 rounded-full" src={noUser} alt="user-image" />
+        <button
+          name={t("_common:names.buttons.userMenu")}
+          aria-label={t("_common:ariaLabels.buttons.userMenu")}
+          onClick={openMenu}
+        >
+          <img className="w-10 rounded-full" src={noUser} alt="user-image" />
+        </button>
       </div>
     </header>
   );
 }
+
+AppBar.propTypes = {
+  openMenu: PropTypes.func,
+};
 
 export default AppBar;
