589b902f4d812f23a48a980259fa168027e5542f
Sito8943 <sito8943@gmail.com>
2024-05-26 08:05:30 -0400

    added sidebar to View layout

--- src/layouts/View.jsx
+++ src/layouts/View.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 // components
 import AppBar from "./AppBar/AppBar";
+import Sidebar from "./Sidebar/Sidebar";
 
 function View() {
+  const [showSidebar, setShowSidebar] = useState();
+
   return (
     <div>
-      <AppBar />
+      <AppBar openMenu={() => setShowSidebar(true)} />
+      <Sidebar open={showSidebar} onClose={() => setShowSidebar(false)} />
       <Outlet />
     </div>
   );
