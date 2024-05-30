96b185632f5a6d02c58aa5c3028be265236c5577
Sito8943 <sito8943@gmail.com>
2024-05-30 17:52:12 -0400

    added modal to type view

--- src/views/Type.jsx
+++ src/views/Type.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import { useDebounce } from "use-lodash-debounce";
@@ -15,6 +15,7 @@ import { InputControl } from "@sito/ui";
 import { css } from "@emotion/css";
 
 import { food } from "../data/types.json";
+import Modal from "../components/Modal";
 
 function Type() {
   const { t } = useTranslation();
@@ -25,8 +26,42 @@ function Type() {
   const handleSearchValue = (e) => setSearchValue(e.target.value);
   const searchValueDebounced = useDebounce(searchValue, 400);
 
+  const [modalFood, setModalFood] = useState();
+  const [showModal, setShowModal] = useState(false);
+  const handleCloseModal = () => {
+    setModalFood();
+  };
+
+  useEffect(() => {
+    if (modalFood) setShowModal(true);
+    else setShowModal(false);
+  }, [modalFood]);
+
+  const modalTitle = useMemo(() => {
+    if (modalFood) {
+      const { type } = modalFood;
+      switch (type) {
+        case "eat":
+          return t("_pages:type.modal.eat", { food: modalFood.plural });
+      }
+    }
+  }, [modalFood, t]);
+
+  const [counter, setCounter] = useState(1);
+
   return (
     <main className="mt-20 p-3 pb-10">
+      <Modal title={modalTitle} open={showModal} handleClose={handleCloseModal}>
+        <InputControl
+          id="country "
+          name={t("_common:names.inputs.counter")}
+          label={t(`_common:types.${modalFood?.type}`)}
+          labelClassName="text-light"
+          value={counter}
+          type="number"
+          onChange={(e) => setCounter(e.target.value)}
+        />
+      </Modal>
       <section id="header" className="w-full">
         <div className="flex flex-col items-start justify-start gap-5 w-full">
           <h2 className="w-full">{t(`_pages:type.types.${type}`)}</h2>
@@ -34,7 +69,7 @@ function Type() {
       </section>
       <section id="search" className="mt-2">
         <InputControl
-          id="user"
+          id="search"
           name={t("_common:names.inputs.search")}
           value={searchValue}
           placeholder={t("_pages:type.search.placeholder")}
@@ -58,7 +93,10 @@ function Type() {
                 })} rounded-md filled primary transition hover:scale-105 ease-in-out duration-300`}
                 key={i}
               >
-                <button className="w-full h-full items-center justify-center p-3 ">
+                <button
+                  onClick={() => setModalFood(food)}
+                  className="w-full h-full items-center justify-center p-3 "
+                >
                   {food.name}
                 </button>
               </li>
