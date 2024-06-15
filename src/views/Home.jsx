b8e5af11a8f944afcc6f1ddaf0b0fa1e12828c20
Sito8943 <sito8943@gmail.com>
2024-06-15 16:02:11 -0400

    memo for home chart

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,18 +1,25 @@
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import { useMemo } from "react";
+
+// icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
+
+// @sito/ui
+import { IconButton } from "@sito/ui";
 
 // components
 import { LPieChart } from "../components/PieChart";
+import Legend from "../components/Legend";
 
 // providers
 import { useAccount } from "../providers/AccountProvider";
-import { useMemo } from "react";
-import Legend from "../components/Legend";
 
 function Home() {
   const { t } = useTranslation();
 
-  const { account } = useAccount();
+  const { account, resetData } = useAccount();
 
   const legend = useMemo(() => {
     return [
@@ -23,44 +30,58 @@ function Home() {
     ];
   }, [t]);
 
+  const dataFromAccount = useMemo(
+    () => [
+      {
+        title: t("_pages:home.points.calories"),
+        value: account?.user?.calories ?? 0,
+        color: "#FF3D22",
+      },
+      {
+        title: t("_pages:home.points.proteins"),
+        value: account?.user?.proteins ?? 0,
+        color: "#0ECC7A",
+      },
+      {
+        title: t("_pages:home.points.lipids"),
+        value: account?.user?.lip ?? 0,
+        color: "#4082DE",
+      },
+      {
+        title: t("_pages:home.points.carbohydrates"),
+        value: account?.user?.carbs ?? 0,
+        color: "#A6A600",
+      },
+    ],
+    [account, t]
+  );
+
   return (
     <main className="w-[90%] mx-auto mt-20 pt-3 pb-10">
       <section id="pie-chart" className="w-full">
         <div className="flex flex-col items-start justify-start gap-5 w-full">
-          <h2 className="text-light text-3xl sm:text-xl">
-            {t("_pages:home.welcome")}{" "}
-            {account?.user?.userName ?? t("_common:noName")}
-          </h2>
+          <div className="flex items-center justify-start gap-2">
+            <h2 className="text-light text-3xl sm:text-xl">
+              {t("_pages:home.welcome")}{" "}
+              {account?.user?.userName ?? t("_common:noName")}
+            </h2>
+            <IconButton
+              name={t("_common:names.buttons.resetStats")}
+              aria-label={t("_common:ariaLabels.buttons.resetStats")}
+              onClick={resetData}
+              color="primary"
+              icon={
+                <FontAwesomeIcon className="text-light" icon={faRotateLeft} />
+              }
+            />
+          </div>
           {account?.user?.calories ||
           account?.user?.proteins ||
           account?.user?.lib ||
-          account?.user?.lib ? (
+          account?.user?.carbs ? (
             <>
               <div className="w-full max-w-75 h-66 m-auto mb-5">
-                <LPieChart
-                  data={[
-                    {
-                      title: t("_pages:home.points.calories"),
-                      value: account?.user?.calories ?? 0,
-                      color: "#FF3D22",
-                    },
-                    {
-                      title: t("_pages:home.points.proteins"),
-                      value: account?.user?.proteins ?? 0,
-                      color: "#0ECC7A",
-                    },
-                    {
-                      title: t("_pages:home.points.lipids"),
-                      value: account?.user?.lip ?? 0,
-                      color: "#4082DE",
-                    },
-                    {
-                      title: t("_pages:home.points.carbohydrates"),
-                      value: account?.user?.carbs ?? 0,
-                      color: "#A6A600",
-                    },
-                  ]}
-                />
+                <LPieChart data={dataFromAccount} />
               </div>
               <div className="flex flex-col items-start justify-start gap-1 mt-10">
                 <h3>{t("_pages:home.legend")}</h3>
